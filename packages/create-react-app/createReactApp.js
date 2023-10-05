23ffc31122e64cf48010c4fbe89cbfeb50e963d3
suzhenghui <343196323@qq.com>
2023-10-05 15:53:12 +0800

    创建run,install,executeNodeScript函数,初步完成创建
    Signed-off-by: suzhenghui <343196323@qq.com>

--- packages/create-react-app/createReactApp.js
+++ packages/create-react-app/createReactApp.js
@@ -3,6 +3,7 @@ const packageJSON = require('./package.json');
 const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs-extra');
+const spawn = require('cross-spawn');
 
 async function init() {
     let projectName;
@@ -37,6 +38,55 @@ async function createApp(appName) { // projectName = appName
     console.log('appName', appName);
     console.log('root', root);
     console.log('originalDirectory', originalDirectory);
+    await run(root, appName, originalDirectory);
+}
+/**
+ * 
+ * @param {*} root 创建的项目的路径
+ * @param {*} appName 项目名
+ * @param {*} originalDirectory 原来的工作目录
+ */
+async function run(root, appName, originalDirectory) {
+    let scriptName = 'react-scripts'; // create生成的代码里 源文件编译，启动服务放在了react-scripts
+    let templateName = 'cra-template';
+    const allDependencies = ['react', 'react-dom', scriptName, templateName];
+
+    console.log('Installing packages. This might take a couple of minutes.');
+    // Installing react, react-dom, and react-scripts with cra-template
+    console.log(
+        `Installing ${chalk.cyan('react')}, ${chalk.cyan(
+            'react-dom'
+        )}, and ${chalk.cyan(scriptName)}${` with ${chalk.cyan(templateName)}`}...`
+    );
+    await install(root, allDependencies);
+    //项目根目录    项目名字   verbose是否显示详细信息 原始的目录 模板名称cra-template
+    let data = [root, appName, true, originalDirectory, templateName];
+    let source = `
+    const init = require('react-scripts/scripts/init.js');
+    init.apply(null, JSON.parse(process.argv[1]));
+    `;
+    await executeNodeScript({ cwd: process.cwd() }, data, source);
+    console.log('Done.');
+    process.exit(0);//退出
+}
+async function executeNodeScript({ cwd }, data, source) {
+    return new Promise((resolve) => {
+        const child = spawn(
+            process.execPath,//node可执行文件的路径
+            ['-e', source, '--', JSON.stringify(data)],
+            { cwd, stdio: 'inherit' }
+        );
+        child.on('close', resolve);
+    });
+}
+async function install(root, allDependencies) {
+    return new Promise(resolve => {
+        const command = 'yarnpkg'; //yarnpkg = yarn
+        const args = ['add', '--exact', ...allDependencies, '--cwd', root];
+        console.log(command, args);
+        const child = spawn(command, args, { stdio: 'inherit' });//stdio = 'inherit' 表示子进程的输出会直接打印到父进程中
+        child.on('close', resolve);
+    })
 }
 
 module.exports = {
